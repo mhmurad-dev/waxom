9d44cb826b0b9334bd2d5522d30ab4db27abcceb
mhmurad-dev <mahedibdcal@gmail.com>
2022-03-02 03:05:25 +0600

    waxom

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+  //=====mixit up start=====/
+  var containerEl = document.querySelector('.pj-body');
+  var mixer = mixitup(containerEl);
+  //=====mixit up end=======/
+
+  //=====counter up js start=====/
+  $('.counter').counterUp({
+    delay: 100,
+    time: 3000,
+});
+  //=====counter up js end=======/
+  //=====mixit up start=====/
+  //=====mixit up end=======/
+  
+})
